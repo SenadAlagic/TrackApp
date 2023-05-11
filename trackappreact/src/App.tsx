6e37e5254293e59d53a615c0036521c5c12122de
Senad Alagić <senad.alagic@edu.fit.ba>
2023-05-11 14:21:20 +0200

    not found added

--- trackappreact/src/App.tsx
+++ trackappreact/src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/home";
+import Details from "./components/Details/details";
+import NotFound from "./components/NotFound/notfound";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/details" element={<Details />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 
